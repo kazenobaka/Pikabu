2d9740a2e5e695572419b428d54aeeff3cf949c8
Алексей <aleksey.pakaa@gmail.com>
2020-12-20 02:44:24 -0800

    Initial Commit

---
+++ Pikabu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pikabu
+//
+//  Created by Алексей on 20.12.2020.
+//  Copyright © 2020 Алексей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
